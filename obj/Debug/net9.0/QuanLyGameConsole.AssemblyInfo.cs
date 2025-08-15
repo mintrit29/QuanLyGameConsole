0b566f0e5ac47c566e642ef579b4a32aab21da8d
mintrit29 <tongminhtriet1967@gmail.com>
2025-08-15 23:29:19 +0700

    xoa cot id

--- obj/Debug/net9.0/QuanLyGameConsole.AssemblyInfo.cs
+++ obj/Debug/net9.0/QuanLyGameConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyGameConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891cba141e253ed639f0e7ae4155cdba4c252d6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eae22deb6accba766e8e54dd1f17348489f72f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyGameConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyGameConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
